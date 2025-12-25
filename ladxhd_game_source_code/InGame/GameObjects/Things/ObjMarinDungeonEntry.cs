921fd50ac25cd96f9c2ba6e29dafde722681cd30
Scott Zimmerman <bighead.0@gmail.com>
2025-12-25 03:42:46 -0500

    Fix Marin dungeon sequence (a.k.a. longstanding Marin beach issue).
    This is at long last hopefully the fix to the Marin beach spawning issue that has plagued this port since the v1.0.0 release. I created a workaround awhile back that seemed to work for the most part, but the issue recently returned and revealed itself to be the dungeon sequence firing when it shouldn't have been.
    
    In earlier versions, Marin would sometimes just straight up disappear after the beach sequence. I created a workaround that respawned her object. Marin is now an "always animated" object, so she can still be rendered and updated outside the screen boundaries. The bug has returned and revealed itself to be the dungeon sequence: aka, she spawns next to the dungeon waiting for Link to come out.
    
    This commit hopefully finally fixes the issue once and for all. It checks if the follower key is set before trying to fire any logic instead of just relying on whether or not her object is currently on the map (because it's always on the map). This also seems to fix another issue where saving outside a dungeon, saving, quitting, and reloading the save would sometimes not respawn Marin. So hopefully all Marin related issues are finally fixed for good.

--- ladxhd_game_source_code/InGame/GameObjects/Things/ObjMarinDungeonEntry.cs
+++ ladxhd_game_source_code/InGame/GameObjects/Things/ObjMarinDungeonEntry.cs
@@ -28,24 +28,40 @@ namespace ProjectZ.InGame.GameObjects.Things
             if (MapManager.ObjLink.NextMapPositionStart == null)
                 return;
 
+            // Check if Link is currently standing in the dungeon entry tile.
             var linkPosition = MapManager.ObjLink.NextMapPositionStart.Value;
             if (_rectangle.Contains(new Point((int)linkPosition.X, (int)linkPosition.Y)))
             {
-                ObjMarin marin = MapManager.ObjLink.GetMarin();
+                // Check if Marin is a follower by checking the key-value pair.
+                var itemMarin = Game1.GameManager.SaveManager.GetString("has_marin");
+                if (!string.IsNullOrEmpty(itemMarin) && itemMarin == "1")
+                {
+                    // If she is, get the NPC version of Marin.
+                    ObjMarin marin = MapManager.ObjLink.GetMarin();
 
-                if (marin != null)
-                    marin.LeaveDungeonSequence(EntityPosition.Position);
+                    // Set the dungeon sequence if she is on the map.
+                    if (marin != null)
+                        marin.LeaveDungeonSequence(EntityPosition.Position);
+                }
             }
         }
 
         private void Update()
         {
+            // Check if Link is currently standing in the dungeon entry tile.
             if (MapManager.ObjLink.BodyRectangle.Intersects(_rectangle))
             {
-                ObjMarin marin = MapManager.ObjLink.GetMarin();
+                // Check if Marin is a follower by checking the key-value pair.
+                var itemMarin = Game1.GameManager.SaveManager.GetString("has_marin");
+                if (!string.IsNullOrEmpty(itemMarin) && itemMarin == "1")
+                {
+                    // If she is, get the NPC version of Marin.
+                    ObjMarin marin = MapManager.ObjLink.GetMarin();
 
-                if (marin != null)
-                    marin.EnterDungeonMessage = true;
+                    // Set the dungeon sequence if she is on the map.
+                    if (marin != null)
+                        marin.EnterDungeonMessage = true;
+                }
             }
         }
     }
